77015dfee287e970c98041146988b9b83de30c04
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2020-11-23 21:14:54 +0200

    Setup updates

--- hands-on-labs/setup/sql/02-create-users.sql
+++ hands-on-labs/setup/sql/02-create-users.sql
@@ -1,6 +1,3 @@
 create user [asaga.sql.highperf] for login [asaga.sql.highperf]
 execute sp_addrolemember 'db_owner', 'asaga.sql.highperf' 
 execute sp_addrolemember 'staticrc80', 'asaga.sql.highperf' 
-    
-CREATE USER [#USER_NAME#] FROM EXTERNAL PROVIDER;
-EXEC sp_addrolemember 'db_owner', '#USER_NAME#'
