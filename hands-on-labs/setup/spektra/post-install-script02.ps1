d80b17ba104528780e71819f19c6dcb0aa849dc9
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2021-03-26 21:17:40 +0530

    Create post-install-script02.ps1

---
+++ hands-on-labs/setup/spektra/post-install-script02.ps1
@@ -0,0 +1,191 @@
+Param (
+  [Parameter(Mandatory = $true)]
+  [string]
+  $azureUsername,
+
+  [string]
+  $azurePassword,
+
+  [string]
+  $azureTenantID,
+
+  [string]
+  $azureSubscriptionID,
+
+  [string]
+  $odlId,
+    
+  [string]
+  $deploymentId
+)
+
+function InstallGit()
+{
+  Write-Host "Installing Git." -ForegroundColor Green -Verbose
+
+  #download and install git...		
+  $output = "$env:TEMP\git.exe";
+  Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.27.0.windows.1/Git-2.27.0-64-bit.exe -OutFile $output; 
+
+  $productPath = "$env:TEMP";
+  $productExec = "git.exe"	
+  $argList = "/SILENT"
+  start-process "$productPath\$productExec" -ArgumentList $argList -wait
+
+}
+
+function InstallAzureCli()
+{
+  Write-Host "Installing Azure CLI." -ForegroundColor Green -Verbose
+
+  #install azure cli
+  Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile C:\AzureCLI.msi -usebasicparsing; 
+  Start-Process msiexec.exe -Wait -ArgumentList '/I C:\AzureCLI.msi /quiet'; 
+  rm C:\AzureCLI.msi
+}
+
+#Disable-InternetExplorerESC
+function DisableInternetExplorerESC
+{
+  $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+  $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+  Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force -ErrorAction SilentlyContinue -Verbose
+  Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green -Verbose
+}
+
+#Enable-InternetExplorer File Download
+function EnableIEFileDownload
+{
+  $HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+  $HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+  Set-ItemProperty -Path $HKLM -Name "1803" -Value 0 -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $HKCU -Name "1803" -Value 0 -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $HKLM -Name "1604" -Value 0 -ErrorAction SilentlyContinue -Verbose
+  Set-ItemProperty -Path $HKCU -Name "1604" -Value 0 -ErrorAction SilentlyContinue -Verbose
+}
+
+#Disable PopUp for network configuration
+Function DisableServerMgrNetworkPopup
+    {
+        cd HKLM:\
+        New-Item -Path HKLM:\System\CurrentControlSet\Control\Network -Name NewNetworkWindowOff -Force 
+
+		Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+    }
+ 
+ #Install edge browser
+ Function InstallEdgeChromium
+    {
+        #Download and Install edge
+        $WebClient = New-Object System.Net.WebClient
+        $WebClient.DownloadFile("http://dl.delivery.mp.microsoft.com/filestreamingservice/files/6d88cf6b-a578-468f-9ef9-2fea92f7e733/MicrosoftEdgeEnterpriseX64.msi","C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi")
+        sleep 5
+        
+	    Start-Process msiexec.exe -Wait '/I C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi /qn' -Verbose 
+        sleep 5
+        $WshShell = New-Object -comObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Azure Portal.lnk")
+        $Shortcut.TargetPath = """C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"""
+        $argA = """https://portal.azure.com"""
+        $Shortcut.Arguments = $argA 
+        $Shortcut.Save()
+
+    }
+#Create InstallAzPowerShellModule
+function InstallAzPowerShellModule
+{
+  Write-Host "Installing Azure PowerShell (NuGet)." -ForegroundColor Green -Verbose
+
+  Install-PackageProvider NuGet -Force
+  Set-PSRepository PSGallery -InstallationPolicy Trusted
+  Install-Module Az -Repository PSGallery -Force -AllowClobber
+}
+
+function InstallAzPowerShellModuleMSI
+{
+  Write-Host "Installing Azure PowerShell (MSI)." -ForegroundColor Green -Verbose
+  #download and install git...		
+  Invoke-WebRequest -Uri https://github.com/Azure/azure-powershell/releases/download/v4.5.0-August2020/Az-Cmdlets-4.5.0.33237-x64.msi -usebasicparsing -OutFile .\AzurePS.msi;
+  Start-Process msiexec.exe -Wait -ArgumentList '/I AzurePS.msi /quiet'; 
+  rm .\AzurePS.msi
+}
+
+#Create-LabFilesDirectory
+function CreateLabFilesDirectory
+{
+  New-Item -ItemType directory -Path C:\LabFiles -force
+}
+
+#Create Azure Credential File on Desktop
+function CreateCredFile($azureUsername, $azurePassword, $azureTenantID, $azureSubscriptionID, $deploymentId)
+{
+  $WebClient = New-Object System.Net.WebClient
+  $WebClient.DownloadFile("https://raw.githubusercontent.com/solliancenet/azure-synapse-analytics-workshop-400/master/artifacts/environment-setup/spektra/AzureCreds.txt","C:\LabFiles\AzureCreds.txt")
+  $WebClient.DownloadFile("https://raw.githubusercontent.com/solliancenet/azure-synapse-analytics-workshop-400/master/artifacts/environment-setup/spektra/AzureCreds.ps1","C:\LabFiles\AzureCreds.ps1")
+
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "ClientIdValue", ""} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzureUserNameValue", "$azureUsername"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzurePasswordValue", "$azurePassword"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzureSQLPasswordValue", "$azurePassword"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzureTenantIDValue", "$azureTenantID"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzureSubscriptionIDValue", "$azureSubscriptionID"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "DeploymentIDValue", "$deploymentId"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"               
+  (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "ODLIDValue", "$odlId"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"  
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "ClientIdValue", ""} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzureUserNameValue", "$azureUsername"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzurePasswordValue", "$azurePassword"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzureSQLPasswordValue", "$azurePassword"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzureTenantIDValue", "$azureTenantID"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzureSubscriptionIDValue", "$azureSubscriptionID"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "DeploymentIDValue", "$deploymentId"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "ODLIDValue", "$odlId"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
+  Copy-Item "C:\LabFiles\AzureCreds.txt" -Destination "C:\Users\Public\Desktop"
+}
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+DisableInternetExplorerESC
+
+EnableIEFileDownload
+
+DisableServerMgrNetworkPopup
+
+InstallEdgeChromium
+
+InstallAzPowerShellModule
+#InstallAzPowerShellModuleMSI
+
+InstallGit
+        
+InstallAzureCli
+
+Uninstall-AzureRm -ea SilentlyContinue
+
+CreateLabFilesDirectory
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
+cd "c:\labfiles";
+
+CreateCredFile $azureUsername $azurePassword $azureTenantID $azureSubscriptionID $deploymentId $odlId
+
+#install sql server cmdlets
+Write-Host "Installing SQL Module." -ForegroundColor Green -Verbose
+Install-Module -Name SqlServer
+
+#install cosmosdb
+Write-Host "Installing CosmosDB Module." -ForegroundColor Green -Verbose
+Install-Module -Name Az.CosmosDB -AllowClobber
+Import-Module Az.CosmosDB
+
+#download the git repo...
+Write-Host "Download Git repo." -ForegroundColor Green -Verbose
+git clone https://github.com/CloudLabsAI-Azure/azure-synapse-analytics-ga-content-packs.git asa
+
+$LabFilesDirectory = "C:\LabFiles"
+
+Stop-Transcript
