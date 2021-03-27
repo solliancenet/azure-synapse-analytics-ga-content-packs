50488787229b4ebe8f4be88ebfe5b507c9f829ea
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2021-03-27 18:30:32 +0530

    Update logontask.ps1

--- hands-on-labs/setup/spektra/logontask.ps1
+++ hands-on-labs/setup/spektra/logontask.ps1
@@ -1,10 +1,19 @@
 Start-Transcript -Path C:\WindowsAzure\Logs\logontasklogs.txt -Append
 
-#cd 'C:\LabFiles\asa\hands-on-labs\setup\automation'
-
 #Install power Bi desktop
 Start-Process -FilePath "C:\LabFiles\PBIDesktop_x64.exe" -ArgumentList '-quiet','ACCEPT_EULA=1'
 
-Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
+Cd 'C:\LabFiles\asa\hands-on-labs\setup\automation'
 
+./environment-setup.ps1
+
+sleep 2
+./lab-01-setup.ps1
+
+sleep 2
+
+./lab-02-setup.ps1
+./lab-03-setup.ps1
+
+Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
 Stop-Transcript
